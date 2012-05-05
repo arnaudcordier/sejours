30bb676209491c066b19b3ce76a21eb567009ccd
ArnAud Cordier <arnaud@mutins.net>
2012-05-05 15:33:17 +0200

    Première vues et premiers templates

--- eedf/urls.py
+++ eedf/urls.py
@@ -1,10 +1,31 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import DetailView, ListView
+from sejours.models import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	#url(r'^$', 'index'),
+	url(r'^convoyage/$',
+		ListView.as_view(
+			queryset=Convoyage.objects.all()[:5],
+			template_name='sejours/tpl/convoyages.html')),
+	url(r'^convoyage/(?P<pk>\d+)\.html$',
+		DetailView.as_view(
+			model=Convoyage,
+			template_name='sejours/tpl/convoyage.html')),
+	url(r'^sejour/(?P<pk>\d+)\.html$',
+		DetailView.as_view(
+			model=Sejour,
+			template_name='tpl/sejour.html')),
+	url(r'^animateur/(?P<pk>\d+)/$',
+		DetailView.as_view(
+			model=Animateur,
+			template_name='tpl/animateur.html')),
+)
+
+urlpatterns += patterns('',
     # Examples:
     # url(r'^$', 'eedf.views.home', name='home'),
     # url(r'^eedf/', include('eedf.foo.urls')),
@@ -15,3 +36,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
