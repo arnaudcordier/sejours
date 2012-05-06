24456755b365f354732220127b4b555fafe2504e
ArnAud Cordier <arnaud@mutins.net>
2012-05-06 18:28:10 +0200

    RequestContext pour les views afin d'avoir le contexte de l'utilisateur dans les vues

--- sejours/views.py
+++ sejours/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from sejours.models import *
+from django.template import RequestContext
 
 def saison(request, saison_id):
 	o = get_object_or_404(Saison, pk=saison_id)
 	convoyages = Convoyage.objects.filter(saison_id = saison_id)
-	return render_to_response('saison.html', {
-			'saison': o,
-			'convoyages': convoyages,
-		})
+	return render_to_response('saison.html',
+			{'saison': o, 'convoyages': convoyages},
+			context_instance=RequestContext(request)
+		)
 
 def convoyage(request, convoyage_id):
 	o = get_object_or_404(Convoyage, pk=convoyage_id)
-	return render_to_response('convoyage.html', {'convoyage': o})
+	return render_to_response('convoyage.html',
+		{'convoyage': o},
+		context_instance=RequestContext(request)
+	)
 
 def index(request):
-	return render_to_response('index.html')
+	return render_to_response('index.html',
+		context_instance=RequestContext(request)
+	)
 	
