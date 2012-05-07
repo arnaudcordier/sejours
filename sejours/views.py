ad3c1c3846ec5be1ab5385c72d61a6b2a46d0913
ArnAud Cordier <arnaud@mutins.net>
2012-05-07 10:59:24 +0200

    Les saisons dans le menu

--- sejours/views.py
+++ sejours/views.py
@@ -6,19 +6,25 @@ def saison(request, saison_id):
 	o = get_object_or_404(Saison, pk=saison_id)
 	convoyages = Convoyage.objects.filter(saison_id = saison_id)
 	return render_to_response('saison.html',
-			{'saison': o, 'convoyages': convoyages},
+	{'lasaison': o, 'convoyages': convoyages, 'lessaisons': lessaisons()},
 			context_instance=RequestContext(request)
 		)
 
 def convoyage(request, convoyage_id):
 	o = get_object_or_404(Convoyage, pk=convoyage_id)
 	return render_to_response('convoyage.html',
-		{'convoyage': o},
+		{'convoyage': o, 'lessaisons': lessaisons()},
 		context_instance=RequestContext(request)
 	)
 
 def index(request):
 	return render_to_response('index.html',
+		{'lessaisons': lessaisons()},
 		context_instance=RequestContext(request)
 	)
-	
+
+# Fonctions utilitaires
+
+def lessaisons(): # TODO: devrait ne renvoyer que les saisons en cours
+	s = Saison.objects.all()
+	return s
