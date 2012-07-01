6291afc758a0bf7dcb7950a6b58e33e92c72ac3b
ArnAud Cordier <arnaud@mutins.net>
2012-07-01 14:12:57 +0200

    Pouvoir effacer un sejour-animateur

--- sejours/views.py
+++ sejours/views.py
@@ -55,22 +55,38 @@ def sejour(request, sejour_id):
 	sejour = get_object_or_404(Sejour, pk=sejour_id)
 	sejours = Sejour.objects.filter(saison__id=sejour.saison_id).order_by('nom')
 	user = getCurrentUser(request)
-
+	peut_creer_sa = False
+	
 	if (peut_voir_sejour(sejour, user)):
 		form_create_animateur = ''
 		if (peut_creer_sejour_animateur(sejour, user)):
+			peut_creer_sa = True
 			sans_directeur = not user.is_superuser
 			form_create_animateur = createAnimateurForm(sejour_id, sans_directeur=sans_directeur)
 			if request.method == 'POST':
-				form_create_animateur = createAnimateurForm(sejour_id, request.POST)
-				if form_create_animateur.is_valid():
-					user = form_create_animateur.save(request)
+				if 'sa_id' in request.POST:
+					sa_id = request.POST['sa_id']
+					sa = SejourAnimateur.objects.get(pk= sa_id)
+					if (int(sa.sejour_id) != int(sejour_id)):
+						logger.error('Tentative d effacer SA ' + str(sa_id) + ' par ' + str(user.id))
+						return redirect('/sejour/' + sejour_id)
+					user_anim = User.objects.get(personne__animateur__id = sa.animateur_id)
+					if (user_anim.id == user.id):
+						logger.error('Tentative d effacer SA ' + str(sa_id) + ' par lui même ' + str(user.id))
+						return redirect('/sejour/' + sejour_id)
+					logger.error('efface sejouranimateur n°' + str(sa_id))
+					sa.delete()
 					return redirect('/sejour/' + sejour_id)
+				else:
+					form_create_animateur = createAnimateurForm(sejour_id, request.POST)
+					if form_create_animateur.is_valid():
+						user = form_create_animateur.save(request)
+						return redirect('/sejour/' + sejour_id)
 	else:
 		return redirect('/')
 
 	return render_to_response('sejour.html',
-		{'sejour': sejour, 'sejours': sejours, 'lessaisons': lessaisons(), 'form_create_animateur': form_create_animateur,},
+		{'sejour': sejour, 'sejours': sejours, 'lessaisons': lessaisons(), 'form_create_animateur': form_create_animateur, 'peut_creer_sa': peut_creer_sa},
 		context_instance=RequestContext(request)
 	)
 
